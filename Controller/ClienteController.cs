dae13f2e5a5ff5f4cdd7a9c690605b2858628141
Phablo Andrade <pablo0.myt@gmail.com>
2016-12-01 15:12:10 -0200

    Controle 2

---
+++ Controller/ClienteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace Controller
+{
+    public class ClienteController
+    {
+        public Endereco buscarEnderecoWS(String cep)
+        {
+            return new EnderecoImpl().buscarEnderecoWebService(cep);
+        }
+    }
+}
