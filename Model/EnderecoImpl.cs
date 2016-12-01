30c5c4b1f5c52f8c826b40f03cb5b4ae4bbc6a19
Phablo Andrade <pablo0.myt@gmail.com>
2016-12-01 15:10:29 -0200

    Camada Modelo

---
+++ Model/EnderecoImpl.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Model
+{
+    public class EnderecoImpl
+    {
+        /**
+         * Método responsavel por buscar o CEP e retornar um endereço
+        **/
+        public Endereco buscarEnderecoWebService(String cep)
+        {
+            Endereco end = new Endereco();
+
+            try
+            {
+                DataSet ds = new DataSet();
+                ds.ReadXml("https://viacep.com.br/ws/" + cep + "/xml/");
+
+                if ( ds != null ) 
+                {
+
+                    if (ds.Tables[0].Rows.Count > 0)
+                    {
+                        end.cep = ds.Tables[0].Rows[0]["cep"].ToString().Trim();
+                        end.logradouro = ds.Tables[0].Rows[0]["logradouro"].ToString().Trim();
+                        end.bairro = ds.Tables[0].Rows[0]["bairro"].ToString().Trim();
+                        end.localidade = ds.Tables[0].Rows[0]["localidade"].ToString().Trim();
+                        end.uf = ds.Tables[0].Rows[0]["uf"].ToString().Trim();
+                    }
+                    else
+                    {
+                        end = null;
+                    }
+                }
+            }
+            catch (ArgumentException e)
+            {
+                end = null;
+            }
+
+            return end;
+        }
+
+    }
+}
+ 
---
+++ Model/Entidades/Endereco.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Endereco
+    {
+        public String cep { get; set; }
+        public String logradouro { get; set; }
+        public String bairro { get; set; }
+        public String localidade { get; set; }
+        public String uf { get; set; }
+    }
+}
